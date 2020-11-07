8f164c8cf0040d0d5e0d947a51f785e7ab68473b
Asmau1234 <amaliyu@hotmail.com>
2020-11-07 13:48:59 +0100

    ..

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,7 @@
 module.exports = function(grunt) {
   // Load grunt tasks automatically
   require('load-grunt-tasks')(grunt);
+
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
